f955aabd834881ff7debd5ace59c87ec65b63814
WANG Nan <nan.i.wang@alcatel-sbell.com.cn>
2012-12-19 18:57:49 +0800

    makeitpossibletoaccesswithoutNTLM

---
+++ application/views/upload_csv.php
@@ -0,0 +1,12 @@
+<?php echo $error;?>
+
+<form action="?category=do_upload" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+
+<input type="file" name="userfile" size="20" />
+
+<br /><br />
+
+<input type="submit" value="upload" />
+
+</form>
+<p> Check <a href="cronjobs/example.csv">here</a> for csv example</p>
