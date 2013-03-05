d0eeee3ede66b3ad6145688ce16371dc63a42025
duduzhu <duduzhu@gmail.com>
2013-03-05 11:01:33 +0800

    reorganize Vampire.pm

--- cronjobs/Vampire/DB.pm
+++ cronjobs/Vampire/DB.pm
@@ -1,22 +1,7 @@
 #!/usr/bin/perl
-package Vampire;
+package DB;
 use strict;
 use DBI;
-#use Mail::Sendmail;
-
-sub num2ip
-{
-    my ($number) = @_;
-    my $IP=sprintf("%08x",$number);
-    $IP =~ /(..)(..)(..)(..)/;
-    return (hex $1).".".(hex $2).".".(hex $3).".".(hex $4);
-}
-sub ip2num
-{
-    my ($ip) = @_;
-    my @ip = split /\./, $ip;
-    return hex sprintf("%02x",$ip[0]).sprintf("%02x",$ip[1]).sprintf("%02x",$ip[2]).sprintf("%02x",$ip[3]);
-}
 
 sub dbinit
 {
